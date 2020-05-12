35ccd25607a5950462e940f3bcd7cc37a61c7d6a
Jess <jlswans1@gmail.com>
2020-05-11 20:06:59 -0700

    guard for using commands out of sequence

--- bot.py
+++ bot.py
@@ -17,24 +17,27 @@ queue = {
 
 @bot.command(name='queue', help='Creates a new queue.  Add a Dodo Code by adding a space after the command, followed by your Dodo Code.  For example: !queue KR16V')
 async def create_queue(ctx, code="None"):
-    # add the dodo code to queue
-    queue['dodo_code'] = code
+    if 'creator' in queue:
+        await ctx.send('There\'s a queue in progress already!  `!endqueue` to end the current queue, and create a new one with `!queue`.')
+    else:
+        # add the dodo code to queue
+        queue['dodo_code'] = code
 
-    # add the creator to queue
-    queue['creator'] = ctx.author
+        # add the creator to queue
+        queue['creator'] = ctx.author
 
-    message = """
-    Queue created by {author}!
+        message = """
+        Queue created by {author}!
 
 Dodo Code: **{code}**
-    """.format(author=ctx.author.mention, code=queue['dodo_code'].upper())
+        """.format(author=ctx.author.mention, code=queue['dodo_code'].upper())
 
-    # send and pin the queue creation message
-    message_to_pin = await ctx.send(message)
-    await message_to_pin.pin()
+        # send and pin the queue creation message
+        message_to_pin = await ctx.send(message)
+        await message_to_pin.pin()
 
-    # add pinned message to queue for later
-    queue['pinned_message'] = message_to_pin
+        # add pinned message to queue for later
+        queue['pinned_message'] = message_to_pin
 
 @bot.command(name='add', help='Adds a user to the queue.  Call the command to add yourself to the queue, or mention a user to add that user to the queue instead.  For example: !add @TomNook')
 async def add_to_queue(ctx, user: discord.Member = None):
@@ -48,41 +51,57 @@ async def add_to_queue(ctx, user: discord.Member = None):
     else:
         # push the user onto the members array
         queue['members'].append(user)
-        response = "{user} has been added to the queue!".format(user=user.mention)
-        await ctx.send(response)
+        message = "{user} has been added to the queue!".format(user=user.mention)
+        await ctx.send(message)
 
         # edit the pinned queue message each time a user gets added
         await add_member_to_pinned_message(user)
 
 @bot.command(name='next', help='Moves to the next user in the queue.')
 async def next_in_queue(ctx):
-    if queue['members']:
-        queue['members'].pop(0)
+    if 'creator' in queue:
+        if queue['members']:
+            queue['members'].pop(0)
 
-    # account for the last member in the queue
-    if not queue['members']:
-        message = "The queue is empty! `!endqueue` will delete this queue, or you can `!add` people to the current queue."
+        # account for the last member in the queue
+        if not queue['members']:
+            message = "The queue is empty! `!endqueue` will delete this queue, or you can `!add` people to the current queue."
+        else:
+            message = "{next_user}, you're up!".format(next_user=queue['members'][0].mention)
     else:
-        message = "{next_user}, you're up!".format(next_user=queue['members'][0].mention)
+        message = "No active queues.  Use `!queue` to start your own!"
+
     await ctx.send(message)
 
 @bot.command(name='endqueue', help='End the currently active queue.')
 async def end_queue(ctx):
-    # unpin message
-    await queue['pinned_message'].unpin()
+    if 'creator' in queue:
+        # unpin message
+        await queue['pinned_message'].unpin()
 
-    # TODO: don't send this if there is no current queue
-    message = "That's a wrap!  {author} has ended the queue.".format(author=ctx.author.mention)
+        # send a confirmation message
+        message = "That's a wrap!  {author} has ended the queue.".format(author=ctx.author.mention)
+
+        queue.clear()
+    else:
+        message = "No active queues.  Use `!queue` to start your own!"
+
     await ctx.send(message)
 
 @bot.command(name='dodo', help='Change the Dodo Code for the queue.  Example: !dodo TT43V')
-async def edit_dodo_code(ctx, code):
-    new_code_string = "Dodo Code: **" + code + "**"
-    new_content = re.sub('Dodo Code: .+', new_code_string, queue['pinned_message'].content)
+async def edit_dodo_code(ctx, code='None'):
+    if 'creator' in queue:
+        new_code_string = "Dodo Code: **" + code + "**"
+        new_content = re.sub('Dodo Code: .+', new_code_string, queue['pinned_message'].content).upper()
 
-    await queue['pinned_message'].edit(content=new_content)
+        # edit the pinned message
+        await queue['pinned_message'].edit(content=new_content)
 
-    message = "Dodo Code updated!"
+        # send a confirmation message
+        message = "Dodo Code updated!"
+    else:
+        message = "No active queues.  Use `!queue` to start your own!"
+
     await ctx.send(message)
 
 # add a new member to the queue
